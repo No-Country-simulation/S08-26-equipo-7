55c2975d69b9294924cfdf2b966533a0973ca1d2
Juan Camilo Garcia <jcgarcia1565@gmail.com>
2026-09-03 22:21:24 -0500

    style: :art: formatin code
    se formateo el codigo bajo las reglas de eslint

--- frontend/src/features/auth/LoginForm.jsx
+++ frontend/src/features/auth/LoginForm.jsx
@@ -26,82 +26,82 @@ export default function LoginForm() {
   return (
     <div className="w-full max-w-100">
       <div className="flex flex-col items-center mb-8 space-y-2">
-          <div className="h-12 w-12 bg-indigo-600 rounded-xl flex items-center justify-center shadow-lg shadow-zinc-900/20">
-            <LayoutDashboard className="w-6 h-6 text-white" />
-          </div>
-          <h1 className="text-2xl font-bold tracking-tight text-slate-700 mt-4">
+        <div className="h-12 w-12 bg-indigo-600 rounded-xl flex items-center justify-center shadow-lg shadow-zinc-900/20">
+          <LayoutDashboard className="w-6 h-6 text-white" />
+        </div>
+        <h1 className="text-2xl font-bold tracking-tight text-slate-700 mt-4">
             ServiceFlow
-          </h1>
-          <p className="text-sm text-slate-500">
+        </h1>
+        <p className="text-sm text-slate-500">
             Ingresa tus credenciales para continuar
-          </p>
-        </div>
-    <form className="space-y-2 md:space-y-4 flex flex-col gap-4 bg-white p-8 rounded-lg shadow-md w-full max-w-md" onSubmit={handleSubmit}>
-      <div className="space-y-2.5">
-        <Label htmlFor="email" className="text-sm font-medium text-slate-700">
+        </p>
+      </div>
+      <form className="space-y-2 md:space-y-4 flex flex-col gap-4 bg-white p-8 rounded-lg shadow-md w-full max-w-md" onSubmit={handleSubmit}>
+        <div className="space-y-2.5">
+          <Label htmlFor="email" className="text-sm font-medium text-slate-700">
           Correo electrónico corporativo
-        </Label>
-        <Input 
-          type="email" 
-          name="email" 
-          id="email" 
-          placeholder="nombre@tuempresa.com"
-          required
-          disabled={isLoading}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          autoComplete="email"
+          </Label>
+          <Input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder="nombre@tuempresa.com"
+            required
+            disabled={isLoading}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="email"
           />
-      </div>
-      <div className="space-y-2.5">
-        <div className="flex items-center justify-between">
-        <Label htmlFor="password" className="text-sm font-medium text-slate-700">
-          Contraseña
-        </Label>
-        <a href="#" className="text-xs font-semibold text-slate-700 hover:underline underline-offset-4">
-          ¿Olvidaste tu contraseña?
-        </a>
         </div>
-        <div className="relative">
-                <Input 
-                  id="password" 
-                  type={showPassword ? "text" : "password"} 
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  disabled={isLoading}
-                  autoComplete="current-password"
-                  className="pr-10" // Espacio para el icono
-                />
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="absolute right-3 top-1/2 -translate-y-1/2 text-slate-400 hover:text-slate-600 focus:outline-none focus:text-slate-900"
-                  tabIndex={-1} // Para que no interfiera en la navegación por tabulador
-                >
-                  {showPassword ? (
-                    <EyeOff className="h-4 w-4" />
-                  ) : (
-                    <Eye className="h-4 w-4" />
-                  )}
-                </button>
-              </div>
-      </div>
-      <Button type="submit" className="w-full bg-indigo-600 hover:bg-indigo-800 cursor-pointer" disabled={isLoading}>
-              {isLoading ? (
-                <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Autenticando...
-                </>
+        <div className="space-y-2.5">
+          <div className="flex items-center justify-between">
+            <Label htmlFor="password" className="text-sm font-medium text-slate-700">
+          Contraseña
+            </Label>
+            <a href="#" className="text-xs font-semibold text-slate-700 hover:underline underline-offset-4">
+          ¿Olvidaste tu contraseña?
+            </a>
+          </div>
+          <div className="relative">
+            <Input 
+              id="password" 
+              type={showPassword ? "text" : "password"} 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              disabled={isLoading}
+              autoComplete="current-password"
+              className="pr-10" // Espacio para el icono
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-1/2 -translate-y-1/2 text-slate-400 hover:text-slate-600 focus:outline-none focus:text-slate-900"
+              tabIndex={-1} // Para que no interfiera en la navegación por tabulador
+            >
+              {showPassword ? (
+                <EyeOff className="h-4 w-4" />
               ) : (
-                "Iniciar Sesión"
+                <Eye className="h-4 w-4" />
               )}
-            </Button>
-    </form>
-    <p className="text-center text-xs text-slate-500 mt-8">
+            </button>
+          </div>
+        </div>
+        <Button type="submit" className="w-full bg-indigo-600 hover:bg-indigo-800 cursor-pointer" disabled={isLoading}>
+          {isLoading ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Autenticando...
+            </>
+          ) : (
+            "Iniciar Sesión"
+          )}
+        </Button>
+      </form>
+      <p className="text-center text-xs text-slate-500 mt-8">
           ServiceFlow es un sistema de uso interno exclusivo.<br/>
           Si necesitas acceso, contacta con tu administrador de área.
-        </p>
+      </p>
     </div>
   );
 }
