4be7105a6a42d956b2b587035ee1ce60b49bac7b
Juan Camilo Garcia <jcgarcia1565@gmail.com>
2026-09-18 13:52:03 -0500

    style: :art: ajust style
    se ajusto el estilo del subtitulo para que fuera mas claro

--- frontend/src/components/InfoBanner.jsx
+++ frontend/src/components/InfoBanner.jsx
@@ -1,9 +1,9 @@
 export default function InfoBanner({ title, paragraph }) {
   return (
     <div className="bg-card border-border my-4 flex rounded-lg border p-4 shadow-md">
-      <div className="mb-4 sm:mb-0">
+      <div className="mb-4 sm:mb-0 space-y-2">
         <h1 className="text-2xl font-bold">{title}</h1>
-        <p className="text-sm text-gray-600 dark:text-gray-300">{paragraph}</p>
+        <p className="text-md text-muted-foreground">{paragraph}</p>
       </div>
     </div>
   );
